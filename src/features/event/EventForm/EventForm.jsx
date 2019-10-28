4a165e26236101c5b4fc99778e29aeb581df247b
jingzhou <jwang245@binghamton.edu>
2019-10-28 16:18:38 -0400

    End of Section 5

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -11,9 +11,24 @@ class EventForm extends Component {
         hostedBy:''
     };
 
+    //A lifecycle diagram to update the states in our event form based on the props that we are receiving
+    componentDidMount() {
+        //First check if there is a selected event in the prop
+        if (this.props.selectedEvent !== null) {
+            this.setState({
+                ...this.props.selectedEvent
+            })
+        }
+    }
+
     handleFormSubmit = (evt) =>{
         evt.preventDefault();
-        this.props.createEvent(this.state);
+        if(this.state.id) {
+            this.props.updateEvent(this.state);
+        }else{
+            this.props.createEvent(this.state);
+        }
+        
     };
 
     handleInputChange = ({target: {name, value}}) => {
