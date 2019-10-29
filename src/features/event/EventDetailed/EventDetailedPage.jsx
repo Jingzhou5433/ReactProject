9cfb328b27ea03526327479459db9abf2515304b
jingzhou <jwang245@binghamton.edu>
2019-10-28 23:06:35 -0400

    End of Section 6

---
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const EventDetailedPage = () => {
+    return (
+        <div>
+            <div>
+                <h1>Event detailed page</h1>
+            </div>
+            
+        </div>
+    )
+}
+
+export default EventDetailedPage
